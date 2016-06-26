3dbdae5eb71fc5f030d7fc45b807b9b517e9e5a3
Maneesh Tewani <mtewani@purdue.edu>
2016-06-25 20:27:29 -0700

    Initial API Request created

--- app/src/main/java/food/grumble/MainActivity.java
+++ app/src/main/java/food/grumble/MainActivity.java
@@ -1,13 +1,59 @@
 package food.grumble;
 
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context ctx = this;
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final Handler handler = new Handler();
+        Thread thread = new Thread() {
+            public void run() {
+                URL url;
+                HttpURLConnection urlConnection = null;
+                try {
+                    url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.914194,-117.086382&radius=2000&type=restaurant&key=<API-KEY>");
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader isw = new InputStreamReader(in);
+                    int data = isw.read();
+                    String finalData = "";
+                    while(data != -1) {
+                        final char current = (char) data;
+                        data = isw.read();
+                        finalData += current;
+                    }
+                    final String finalData1 = finalData;
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(finalData1);
+                        }
+                    });
+
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
     }
+
 }
